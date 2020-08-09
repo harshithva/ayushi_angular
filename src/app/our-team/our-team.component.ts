12652a0dff54d2159e53a8f6afe5896da2e47cde
amolsr <amol.saini567@gmail.com>
2020-08-09 15:06:41 +0530

    corrections

--- src/app/our-team/our-team.component.ts
+++ src/app/our-team/our-team.component.ts
@@ -1,27 +1,28 @@
-import { Comments } from './../comments';
-import { ApiService } from './../api.service';
-import { Component, OnInit } from '@angular/core';
+import { Comments } from "./../comments";
+import { ApiService } from "./../api.service";
+import { Component, OnInit } from "@angular/core";
+import { ParticlesConfig } from "../landing/particles";
+
+declare var particlesJS: any;
 
 @Component({
-  selector: 'app-our-team',
-  templateUrl: './our-team.component.html',
-  styleUrls: ['./our-team.component.css']
+  selector: "app-our-team",
+  templateUrl: "./our-team.component.html",
+  styleUrls: ["./our-team.component.css"],
 })
 export class OurTeamComponent implements OnInit {
-
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {}
   results: Comments[];
 
   ngOnInit(): void {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-    this.apiService.getcomments()
-    .subscribe(
-      data => {
-        this.results = data.data;
-        console.log(this.results);
-      }
-    );
+    this.apiService.getcomments().subscribe((data) => {
+      this.results = data.data;
+      console.log(this.results);
+    });
+    particlesJS("particles-js", ParticlesConfig, function () {
+      console.log("callback - particles.js config loaded");
+    });
   }
-
 }
