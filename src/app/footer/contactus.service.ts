7c95b73c5a7a3aaa891c91cff76204102f4a9582
amolsr <amol.saini567@gmail.com>
2020-08-13 18:35:33 +0530

    positioning

--- src/app/footer/contactus.service.ts
+++ src/app/footer/contactus.service.ts
@@ -6,16 +6,16 @@ import { HttpClient } from "@angular/common/http";
 })
 export class ContactUsService {
   data = "any";
-  data2= "any"
+  data2 = "any";
   // tslint:disable-next-line: variable-name
   _url = "https://brl-apis.herokuapp.com/contact-us/";
-  registerURL="https://brl-apis.herokuapp.com/registerme";
-  constructor(private _http: HttpClient,
-              private _http2: HttpClient,) {}
+  registerURL = "https://brl-apis.herokuapp.com/registerme/";
+  constructor(private _http: HttpClient, private _http2: HttpClient) {}
 
   register(data) {
     return this._http.post<any>(this._url, data);
   }
   registration(data2) {
     return this._http2.post<any>(this.registerURL, data2);
-}}
+  }
+}
